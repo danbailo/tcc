40b7d290a4aff57c9e52a4d7ac86dc53c0c429ed
Daniel Bailo <danbailoufms@gmail.com>
2021-09-06 13:27:50 -0300

    documento no banco de dados responsavel por armazenar as respostas sinteticas dos usuarios

---
+++ src/core/models.py
@@ -0,0 +1,19 @@
+from mongoengine import BooleanField, StringField
+
+
+class Answer:
+    def __init__(Document):
+        """ Armazena as respostas do usuário no banco de dados """
+        _id = StringField(primary_key=True, required=True) #mongodb autoimplemnt this field
+        answer_1 = BooleanField(required=True)
+        answer_2 = BooleanField(required=True)
+        answer_3 = BooleanField(required=True)
+        answer_4 = BooleanField(required=True)
+        answer_5 = BooleanField(required=True)
+        answer_6 = BooleanField(required=True)
+        answer_7 = BooleanField(required=True)
+        answer_8 = BooleanField(required=True)
+        answer_9 = BooleanField(required=True)
+        answer_10 = BooleanField(required=True)
+        answer_11 = BooleanField(required=True)
+        user_id = StringField(unique=True, required=True)
