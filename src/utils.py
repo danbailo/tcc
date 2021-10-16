50e924a83b9a00813437f6bead7b99e22295df56
Daniel Bailo <danbailoufms@gmail.com>
2021-10-16 15:47:06 -0300

    docs: adicionada documentacao das funcoes

--- src/utils.py
+++ src/utils.py
@@ -11,7 +11,12 @@ random.seed(42)
 
 
 def gen_randomic_answers(number_of_answers: int) -> None:
-    """Gera "n" respostas sintéticas e armazena no banco de dados"""
+    """Gera 'n' respostas sintéticas e armazena no banco de dados
+
+    Args:
+        number_of_answers (int): número de respostas para serem
+            adicionadas no banco de dados.
+    """
     for _ in range(number_of_answers):
         answer = Answer(
             answer_1=random.choice([True, False]),
@@ -28,17 +33,19 @@ def gen_randomic_answers(number_of_answers: int) -> None:
             evaded=random.choice([True, False]),
         )
         answer.save()
-    print(f"Gerado {number_of_answers} respostas sintéticas!")
+    print(f'Gerado {number_of_answers} respostas sintéticas!')
 
 
 def mongo_to_df() -> pd.DataFrame:
     """Pega os dados da coleção de respostas do banco de dados e converte num
-    dataframe"""
+    json, então, é criado uma lista de dicionários para a criação de um
+    dataframe.
+
+    Returns:
+        pd.DataFrame: dataframe contendo os dados da base de dados do projeto.
+    """
     temp_data = []
     for answer in Answer.objects:
-        # Pega cada documento da coleção e converte esse em json, depois cada
-        # json é convertido num dicionário e é criado uma lista de dicionarios
-        # para a criação de um dataframe
         temp_data.append(json.loads(answer.to_json()))
     return pd.DataFrame(temp_data)
 
@@ -46,16 +53,34 @@ def mongo_to_df() -> pd.DataFrame:
 def classification_report_as_df(
             y_test: np.array, y_pred: np.array
         ) -> pd.DataFrame:
+    """Transforma o relatório dos modelos de classificação num dataframe.
+
+    Args:
+        y_test (np.array): dados de teste do modelo de classificação.
+        y_pred (np.array): dados preditos do modelo de classificação.
+
+    Returns:
+        pd.DataFrame: relatório do modelo de classificação.
+    """
     return pd.DataFrame(classification_report(
         y_test, y_pred, output_dict=True)
     ).T
 
 
 def data_augmentation(df: pd.DataFrame, len_of_answers: int) -> pd.DataFrame:
+    """Duplica a quantidade de colunas do banco de dados de forma sintética.
+
+    Args:
+        df (pd.DataFrame): dataframe contendo os dados iniciais.
+        len_of_answers (int): número de colunas do dataframe.
+
+    Returns:
+        pd.DataFrame: relatório do modelo de classificação.
+    """
     evaded = df['evaded'].copy()
     df = df[df.columns.drop('evaded')]
     temp = []
-    for i in range(len_of_answers+1, (len_of_answers*2)+1):
+    for i in range(len_of_answers, (len_of_answers*2)):
         for _ in range(len(df)):
             temp.append(random.choice([True, False]))
         df['answer_'+str(i)] = temp.copy()
@@ -65,6 +90,16 @@ def data_augmentation(df: pd.DataFrame, len_of_answers: int) -> pd.DataFrame:
 
 
 def expand_rows(df: pd.DataFrame, size: int = 1) -> pd.DataFrame:
+    """Duplica a quantidade de colunas do banco de dados de forma sintética.
+
+    Args:
+        df (pd.DataFrame): dataframe contendo os dados iniciais.
+        size (int): número de linhas que serão adicionadas, onde
+            cada unidade é multiplicada pelo tamanho atual do dataframe.
+
+    Returns:
+        pd.DataFrame: novo dataframe com linhas sintéticas adicionadas.
+    """
     for i in range(len(df), len(df)+(len(df)*size)):
         df.loc[i] = [random.choice([True, False]) for _ in range(len(
             df.columns))
