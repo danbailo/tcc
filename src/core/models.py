c1d4bf202db0e586d37f8c000e865b2ae6f34729
Daniel Bailo <danbailoufms@gmail.com>
2021-10-16 16:01:55 -0300

    style: flake8

--- src/core/models.py
+++ src/core/models.py
@@ -1,17 +1,17 @@
-from mongoengine import BooleanField, Document, StringField
+from mongoengine import BooleanField, Document
 
 
 class Answer(Document):
-        """ Armazena as respostas do usuário no banco de dados """
-        answer_1 = BooleanField(required=True)
-        answer_2 = BooleanField(required=True)
-        answer_3 = BooleanField(required=True)
-        answer_4 = BooleanField(required=True)
-        answer_5 = BooleanField(required=True)
-        answer_6 = BooleanField(required=True)
-        answer_7 = BooleanField(required=True)
-        answer_8 = BooleanField(required=True)
-        answer_9 = BooleanField(required=True)
-        answer_10 = BooleanField(required=True)
-        answer_11 = BooleanField(required=True)
-        evaded = BooleanField(required=True)
+    """ Armazena as respostas do usuário no banco de dados """
+    answer_1 = BooleanField(required=True)
+    answer_2 = BooleanField(required=True)
+    answer_3 = BooleanField(required=True)
+    answer_4 = BooleanField(required=True)
+    answer_5 = BooleanField(required=True)
+    answer_6 = BooleanField(required=True)
+    answer_7 = BooleanField(required=True)
+    answer_8 = BooleanField(required=True)
+    answer_9 = BooleanField(required=True)
+    answer_10 = BooleanField(required=True)
+    answer_11 = BooleanField(required=True)
+    evaded = BooleanField(required=True)
--- src/database.py
+++ src/database.py
@@ -5,31 +5,33 @@ from mongoengine import connect, disconnect_all
 from .core.models import Answer
 from .utils import gen_randomic_answers
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--number", "-n",
-        help="Número de respostas sintéticas que serão geradas",
+        '--number', '-n',
+        help='Número de respostas sintéticas que serão geradas',
         type=int
     )
     parser.add_argument(
-        "--drop", "-d",
-        help="Deleta a coleção que armazena as respostas",
-        action="store_true",
+        '--drop', '-d',
+        help='Deleta a coleção que armazena as respostas',
+        action='store_true',
     )
     args = parser.parse_args()
 
     if not(args.number or args.drop):
         # Força o usuário entrar com pelo menos um argumento
-        parser.error("Nenhuma ação requisitada, selecione entre --number ou --drop")
+        parser.error(
+            'Nenhuma ação requisitada, selecione entre --number ou --drop'
+        )
 
-    connect(db="tcc")
-    
+    connect(db='tcc')
+
     if args.number:
-        # Pega o número passado no argumento para gerar "n" respostas
+        # Pega o número passado no argumento para gerar 'n' respostas
         gen_randomic_answers(args.number)
-    
+
     if args.drop:
         # Deleta a coleção que armazena as respotas
         print('A coleção "Anwser" foi deletada!')
