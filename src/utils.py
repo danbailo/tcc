7b15dd9f6bbcce94a3d5252cc1aa06fcd732965a
Daniel Bailo <danbailoufms@gmail.com>
2021-10-16 15:17:35 -0300

    style: flake8

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import json
 import random
 
+import numpy as np
 import pandas as pd
 from sklearn.metrics import classification_report
 
@@ -8,40 +9,49 @@ from .core.models import Answer
 
 random.seed(42)
 
-def gen_randomic_answers(number_of_answers):
-    """ Gera "n" respostas sintéticas e armazena no banco de dados """
+
+def gen_randomic_answers(number_of_answers: int) -> None:
+    """Gera "n" respostas sintéticas e armazena no banco de dados"""
     for _ in range(number_of_answers):
         answer = Answer(
-            answer_1 = random.choice([True, False]),
-            answer_2 = random.choice([True, False]),
-            answer_3 = random.choice([True, False]),
-            answer_4 = random.choice([True, False]),
-            answer_5 = random.choice([True, False]),
-            answer_6 = random.choice([True, False]),
-            answer_7 = random.choice([True, False]),
-            answer_8 = random.choice([True, False]),
-            answer_9 = random.choice([True, False]),
-            answer_10 = random.choice([True, False]),
-            answer_11 = random.choice([True, False]),
-            evaded = random.choice([True, False]),
+            answer_1=random.choice([True, False]),
+            answer_2=random.choice([True, False]),
+            answer_3=random.choice([True, False]),
+            answer_4=random.choice([True, False]),
+            answer_5=random.choice([True, False]),
+            answer_6=random.choice([True, False]),
+            answer_7=random.choice([True, False]),
+            answer_8=random.choice([True, False]),
+            answer_9=random.choice([True, False]),
+            answer_10=random.choice([True, False]),
+            answer_11=random.choice([True, False]),
+            evaded=random.choice([True, False]),
         )
         answer.save()
     print(f"Gerado {number_of_answers} respostas sintéticas!")
 
-def mongo_to_df():
-    """ Pega os dados da coleção de respostas do banco de dados e converte num dataframe """
+
+def mongo_to_df() -> pd.DataFrame:
+    """Pega os dados da coleção de respostas do banco de dados e converte num
+    dataframe"""
     temp_data = []
     for answer in Answer.objects:
-        # Pega cada documento da coleção e converte esse em json, depois cada json
-        # é convertido num dicionário e é criado uma lista de dicionarios para
-        # a criação de um dataframe
+        # Pega cada documento da coleção e converte esse em json, depois cada
+        # json é convertido num dicionário e é criado uma lista de dicionarios
+        # para a criação de um dataframe
         temp_data.append(json.loads(answer.to_json()))
     return pd.DataFrame(temp_data)
 
-def classification_report_as_df(y_test, y_pred):
-    return pd.DataFrame(classification_report(y_test, y_pred, output_dict=True)).T    
 
-def data_augmentation(df, len_of_answers):
+def classification_report_as_df(
+            y_test: np.array, y_pred: np.array
+        ) -> pd.DataFrame:
+    return pd.DataFrame(classification_report(
+        y_test, y_pred, output_dict=True)
+    ).T
+
+
+def data_augmentation(df: pd.DataFrame, len_of_answers: int) -> pd.DataFrame:
     evaded = df['evaded'].copy()
     df = df[df.columns.drop('evaded')]
     temp = []
@@ -53,7 +63,10 @@ def data_augmentation(df, len_of_answers):
     df['evaded'] = evaded
     return df
 
-def expand_rows(df, size=1):
+
+def expand_rows(df: pd.DataFrame, size: int = 1) -> pd.DataFrame:
     for i in range(len(df), len(df)+(len(df)*size)):
-        df.loc[i] = [random.choice([True, False]) for _ in range(len(df.columns))]
+        df.loc[i] = [random.choice([True, False]) for _ in range(len(
+            df.columns))
+        ]
     return df
